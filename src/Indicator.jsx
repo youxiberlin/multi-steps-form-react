7e15983259755c1a488794ec2b1b109eb31930d6
Yuki Sato <sato.youxi@gmail.com>
2018-11-04 18:18:24 +0100

    simplified the component

--- src/Indicator.jsx
+++ src/Indicator.jsx
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Indicator extends Component {
-  calcWidth(step) {
-    let width = 0;
-    for (let i = 1; i <= step; i++) {
-      width += 100 / 6
-    }
-    return width;
+const Indicator = props => {
+  const step = props.step;
+  let number = (100 / 6) * step
+
+  const indicatorStyle = {
+    width: number + "%",
+    height: "3vh",
+    background: "rgb(169, 180, 204)"
   }
 
-  render() {
-    const step = this.props.step;
-    let number = this.calcWidth(step);
-
-    const indicatorStyle = {
-      width: number + "%",
-      height: "3vh",
-      background: "rgb(169, 180, 204)"
-    }
-
-    return (
-      <div className="indicator" style={indicatorStyle}></div>
-    );
-  }
+  return (
+    <div className="indicator" style={indicatorStyle}></div>
+  );
 }
+
 
 export default Indicator;
